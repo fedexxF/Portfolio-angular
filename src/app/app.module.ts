953d4946a48eae8c10156511dd78288ca5ef8a32
fedexxF <fefedeq@gmail.com>
2022-05-31 15:54:38 -0300

    Portfolio Angular c:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { BodyComponent } from './components/body/body.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    BodyComponent,
   ],
   imports: [
     BrowserModule
